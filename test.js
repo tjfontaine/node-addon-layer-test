0b4b133cf36af1ea28113fd75f05767071b0919f
Timothy J Fontaine <tjfontaine@gmail.com>
2013-08-01 16:11:36 -0700

    use tt for test output

--- test.js
+++ test.js
@@ -10,87 +10,90 @@ if (!/v0.8/.test(process.version)) {
   setInterval(_gc, 1).unref();
 }
 
+var tt = require('tt');
 var b = require('bindings')('addon_test');
 
-console.log(b);
-
-
-function test1() {
-  console.log("calling test_func");
-  console.log("test_func ret", b.test_func(40, 44, "baz"));
-}
-
-function test2() {
-  console.log("test_foo ret", b.test_foo("hello world"));
-}
-
-function test3() {
-  console.log("test_cb ret", b.test_cb(function () {
-    _gc();
-    console.log("in cb");
-    return "barbarella";
-  }));
-}
-
-function test4() {
-  console.log("calling into test_async");
-
-  b.test_cb_async(function(foo) {
-    _gc();
-    console.log("asyncd", foo);
+function test(name, cb) {
+  return tt(name, function(t) {
+    var _end = t.end;
+    t.end = function() {
+      _gc();
+      _end();
+    };
+    cb(t);
     _gc();
   });
-
-  console.log("tick");
 }
 
-function test5() {
+test('test_func', function(t) {
+  console.log("test_func ret", b.test_func(40, 44, "baz"));
+  t.strictEqual(40, b.test_func(40, 44, 'baz'));
+  t.end();
+});
+
+test('test_foo', function(t) {
+  t.strictEqual('Goodbye Fool', b.test_foo('Hello World'));
+  t.end();
+});
+
+test('test_cb', function(t) {
+  t.strictEqual('barbarella', b.test_cb(function () {
+    return "barbarella";
+  }));
+  t.end();
+})
+
+test('test_cb_async', function(t) {
+  var tick = false;
+  b.test_cb_async(function(foo) {
+    t.strictEqual(tick, true);
+    _gc();
+    t.strictEqual(42, foo);
+    _gc();
+    t.end();
+  });
+  tick = true;
+});
+
+test('test_make_weak', function(t) {
   var a = {};
   a.weak = b.test_weak({a:42})
 
   setTimeout(function() {
-    console.log('clearing out weak', a.weak);
     delete a.weak;
     a.weak = null;
     _gc();
+    t.end();
   }, 0);
-}
-
-function test6() {
-  console.log("test_str", b.test_str());
-}
-
-function test7() {
-  var helloworld = new Buffer("hello world");
-  console.log("test_pass_buff", b.test_pass_buff(helloworld));
-}
-
-function test8() {
-  console.log("test_undefined", b.test_undefined());
-}
-
-function test9() {
-  console.log("test_null", b.test_null());
-}
-
-function test10() {
-  console.log("test_cb_null", b.test_cb_null(function(arg) {
-    console.log("test_cb_null arg", arg);
-  }));
-}
-
-setInterval(function () {
   _gc();
-  test1();
-  test2();
-  test3();
-  test4();
-  test5();
-  test6();
-  test7();
-  test8();
-  test9();
-  test10();
-  _gc();
-}, 1000);
+});
 
+test('test_str', function(t) {
+  t.strictEqual('wtf', b.test_str());
+  t.end();
+});
+
+test('test_pass_buff', function(t) {
+  var contents = 'hello world'
+  var helloworld = new Buffer(contents);
+  var ret = b.test_pass_buff(helloworld);
+  t.strictEqual(ret, contents);
+  t.end();
+})
+
+test('test_undefined', function(t) {
+  t.strictEqual(b.test_undefined(), undefined);
+  t.end();
+});
+
+test('test_null', function(t) {
+  t.strictEqual(b.test_null(), null);
+  t.end();
+});
+
+test('test_cb_null', function(t) {
+  b.test_cb_null(function(arg) {
+    t.strictEqual(arg, null);
+    t.end();
+  });
+});
